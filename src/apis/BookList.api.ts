c8904024442138eb88381f6ce3eea467495b3e62
IDKNWHORU <junho9341@gmail.com>
2023-06-14 14:46:30 +0900

    Implement API test suite using Jest for comprehensive API testing

--- src/apis/BookList.api.ts
+++ src/apis/BookList.api.ts
@@ -1,8 +1,26 @@
-export const getBookList = async (keyword: string, pageNumber?: string) => {
-    const baseUrl = `https://api.itbook.store/1.0/search/${keyword}`;
-    const bookListUrl = (pageNumber) ? `https://api.itbook.store/1.0/search/${keyword}`.concat(`/${pageNumber}`): baseUrl;
-    
-    const response = await fetch(bookListUrl);
-    const data = await response.json();
-    return data;
+import { BookAPIRequest } from "./Request";
+
+export const BookList = class {
+    private readonly request: BookAPIRequest;
+
+    private constructor(request: BookAPIRequest) {
+        this.request = request;
+    }
+
+    static getInstance(request: BookAPIRequest) {
+        return new BookList(request);
+    }
+
+    async getBookList(keyword: string, pageNumber?: string) {
+        const baseUrl = `https://api.itbook.store/1.0/search/${keyword}`;
+        const bookListUrl = (pageNumber) ? `https://api.itbook.store/1.0/search/${keyword}`.concat(`/${pageNumber}`): baseUrl;
+        
+        const response = await this.request.get(bookListUrl);
+        const data = await response.json();
+
+        if(data.error !== "0") {
+            throw new Error(data.error);
+        }
+        return data;
+    }
 }
--- src/tests/BookList.test.tsx
+++ src/tests/BookList.test.tsx
@@ -1,6 +1,27 @@
-import { getBookList } from "../apis/BookList.api";
+import fetch from "node-fetch";
+import { BookList } from "../apis/BookList.api";
+import { APIRequest, BookAPIRequest } from "../apis/Request";
 
-test("hello jest", async () => {
-    const bookList = await getBookList("mongodb");
-    expect(bookList).toEqual([]);
-})
+class FetchAPI implements APIRequest {
+  get(url: string): Promise<any> {
+    return fetch(url);
+  }
+}
+
+test("search book list [keywords: mongodb]", async () => {
+  const bookTestApiRequest = BookAPIRequest.of(new FetchAPI());
+  const bookListRes = await BookList.getInstance(
+    bookTestApiRequest
+  ).getBookList("mongodb");
+
+  expect(bookListRes.error).toBe("0");
+  expect(bookListRes.books.length > 0).toBe(true);
+});
+
+test("search book list empty keyword throw error", async () => {
+  const bookTestApiRequest = BookAPIRequest.of(new FetchAPI());
+
+  await expect(async () => {
+    await BookList.getInstance(bookTestApiRequest).getBookList("");
+  }).rejects.toThrowError(new Error("[search] Invalid request"));
+});
