39b9797b17b167fd016f931a958ea6986ca23957
IDKNWHORU <junho9341@gmail.com>
2023-06-15 15:16:09 +0900

    Add book list page

---
+++ src/apis/models/Library.type.ts
@@ -0,0 +1,6 @@
+export interface Book {
+    title: string;
+    subtitle: string;
+    image: string;
+    url: string;
+}
--- src/tests/BookList.test.tsx
+++ src/tests/BookList.test.tsx
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import { BookList } from "../apis/BookList.api";
 import { APIRequest, BookAPIRequest } from "../apis/Request";
+import { Book } from "../apis/models/Library.type";
 
 class FetchAPI implements APIRequest {
   get(url: string): Promise<any> {
@@ -10,18 +11,17 @@ class FetchAPI implements APIRequest {
 
 test("search book list [keywords: mongodb]", async () => {
   const bookTestApiRequest = BookAPIRequest.of(new FetchAPI());
-  const bookListRes = await BookList.getInstance(
+  const mongodbBooks = await BookList.getInstance(
     bookTestApiRequest
-  ).getBookList("mongodb");
+  ).getBookList<Book>("mongodb");
 
-  expect(bookListRes.error).toBe("0");
-  expect(bookListRes.books.length > 0).toBe(true);
+  expect(mongodbBooks.length > 0).toBe(true);
 });
 
 test("search book list empty keyword throw error", async () => {
   const bookTestApiRequest = BookAPIRequest.of(new FetchAPI());
 
   await expect(async () => {
-    await BookList.getInstance(bookTestApiRequest).getBookList("");
+    await BookList.getInstance(bookTestApiRequest).getBookList<Book>("");
   }).rejects.toThrowError(new Error("[search] Invalid request"));
 });
